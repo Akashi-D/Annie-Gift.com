62f5589cde2b2ccf72dd4a3ab4a97cbd2434868a
Akashi-D <david.okwuzi@yahoo.com>
2022-11-29 06:43:53 +0100

    first commit

---
+++ style.js
@@ -0,0 +1,47 @@
+function myFunction() {
+    var x = document.getElementById('location');
+    if(x.style.display === 'none'){
+        x.style.display='block'
+    }else{
+        x.style.display = 'none'
+    }
+}
+
+function myFunction1() {
+    var x = document.getElementById('location1');
+    if(x.style.display === 'none'){
+        x.style.display='block'
+    }else{
+        x.style.display = 'none'
+    }
+}
+
+function myFunction2() {
+    var x = document.getElementById('location2');
+    if(x.style.display === 'none'){
+        x.style.display='block'
+    }else{
+        x.style.display = 'none'
+    }
+}
+
+function myFunction3() {
+    var x = document.getElementById('wishes');
+    if(x.style.display === 'none'){
+        x.style.display='block'
+    }else{
+        x.style.display = 'none'
+    }
+}
+
+function myFunction6(){
+    document.getElementById('wishes').style.display = 'none'
+}
+
+function myFunction4() {
+    document.getElementById('Acc').style.display = 'block'
+}
+
+function myFunction5() {
+    document.getElementById('Acc').style.display = 'none'
+}
